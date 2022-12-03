c51f5058c659bc2fff025a3abd2d9827a4ccbf2f
LiamSo121 <liam121300@gmail.com>
2022-12-03 16:11:08 +0200

    Update

--- talib_utilities/Indicators.py
+++ talib_utilities/Indicators.py
@@ -9,12 +9,13 @@ def calculate_all_indicators():
     df = pd.DataFrame()
     # Relevant only if pulling data from yFinance 
     df = df.ta.ticker("AAPL",period= '1month',interval = '1m')[['Open','High','Low','Close','Volume']]
-    # if data is from Database - take only Open High Low Close Volume in order to calculate all indicators
-
-    df['MACD'] = ta.macd(df.Close, fast=12, slow=26, signal=9, append= True,fill_na='-')['MACD_12_26_9']
+    # macd requires at least 26 candles
+    df['MACD'] = ta.macd(df.Close, fast=12, slow=26, signal=9, append= True)['MACD_12_26_9']
     df['VWAP'] = calculate_vwap(df)
     df['RSI'] = ta.rsi(df.Close,length=14,fillna= '-')
-    print(df)
+    df['SMA'] = ta.sma(df.Close,length=14)
+    df['EMA'] = ta.ema(df.Close,length=14)
+    return df
 
 def calculate_vwap(df):
     df.set_index(pd.DatetimeIndex(df.index), inplace=True)
