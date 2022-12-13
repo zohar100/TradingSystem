5bb0217b54d017646d37c4f05594229d764fbe5c
Zohar Banai <zoarb100@gmail.com>
2022-12-13 21:46:01 +0200

    Update extended_talib.py

--- strategies/gap_reversal/extended_talib.py
+++ strategies/gap_reversal/extended_talib.py
@@ -1,5 +1,6 @@
 import talib
 
+# BUY -> SELL and SELL -> BUY
 SUPER_CDL3LINESTRIKE = getattr(talib, "CDL3LINESTRIKE")
 def NEW_CDL3LINESTRIKE(open: float, high: float, low: float, close: float):
     results = SUPER_CDL3LINESTRIKE(open, high, low, close)
@@ -10,3 +11,14 @@ def NEW_CDL3LINESTRIKE(open: float, high: float, low: float, close: float):
             results[i] = 100
     return results
 talib.CDL3LINESTRIKE = NEW_CDL3LINESTRIKE
+
+
+# ONLY SELL DETECTION
+SUPER_CDLGRAVESTONEDOJI = getattr(talib, "CDLGRAVESTONEDOJI")
+def NEW_CDLGRAVESTONEDOJI(open: float, high: float, low: float, close: float):
+    results = SUPER_CDLGRAVESTONEDOJI(open, high, low, close)
+    for i,res in results.items():
+        if res != -100:
+            results[i] = 0
+    return results
+talib.CDLGRAVESTONEDOJI = NEW_CDLGRAVESTONEDOJI
