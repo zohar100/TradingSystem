194dd89c93d1f8c110800ed90409c2aeb302fc58
Zohar Banai <zoarb100@gmail.com>
2023-01-31 20:44:20 +0200

    Update start_gap_reversal.py

--- start_gap_reversal.py
+++ start_gap_reversal.py
@@ -3,8 +3,8 @@ from datetime import date
 from strategies.gap_reversal.gap_reversal import gap_reversal
 
 
-start_date = date(2022, 12, 4)
-end_date = date(2022, 12, 4)
+start_date = date(2022, 12, 6)
+end_date = date(2022, 12, 6)
 
 gap_reversal_test = gap_reversal(start_date, end_date)
 
