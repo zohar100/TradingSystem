f2e7762df97177c0d8ca658ce307dc91dad4c964
Zohar Banai <zoarb100@gmail.com>
2023-02-04 19:37:21 +0200

    basics of data class

---
+++ data/data_manipulator.py
@@ -0,0 +1,19 @@
+
+
+class data:
+    
+    @staticmethod
+    def read():
+        pass
+    
+    @staticmethod
+    def write():
+        pass
+    
+    @staticmethod
+    def edit():
+        pass
+
+    @staticmethod
+    def delete():
+        pass
